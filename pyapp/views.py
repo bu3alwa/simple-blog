ced14bd3ddd1bf12894d657db5c4d1f0eda7b1e6
madmanali93 <ali.bu3alwa@gmail.com>
2015-04-03 03:29:23 -0500

    fixed index and login templates

--- pyapp/views.py
+++ pyapp/views.py
@@ -5,7 +5,7 @@ from flask import render_template, request, session, redirect, session, Blueprin
 from pyapp import app
 from models import *
 from pyapp import db
-#from jinja2 import TemplateNotFound
+from jinja2 import TemplateNotFound
 
 SESSION_AGE = 600
 INVALID_SESSION = ("Your session expired, pleade log in again", 403)
@@ -21,9 +21,22 @@ def index():
     posts = User.query.order_by(User.id).all()
     return render_template("index.html", posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return app.root_path
+    if request.method == 'POST':
+        user_name = request.args.get('user').strip()
+        password = request.args.get('pass')
+        try:
+            user = User.query.filter_by(username=user_name).first()
+            if user.verify_pass(password):
+                return "logged in"
+            else:
+                return render_template("login.html")
+        except:
+            return "login error"
+        
+    elif request.method == 'GET':
+         return render_template("login.html")
 
 @app.route('/create-user')
 def create_user():
