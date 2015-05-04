ec7eeac508fefaf09c5891a430ab8a57c08ac385
madmanali93 <ali.bu3alwa@gmail.com>
2015-05-04 16:45:25 -0500

    Update composer.sh

--- composer.sh
+++ composer.sh
@@ -1,3 +1,5 @@
-$ curl -sS https://getcomposer.org/installer | php
-$ sudo mv composer.phar /usr/local/bin/composer
-$ sudo chmod +x /usr/local/bin/composer
+#!/bin/bash
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
